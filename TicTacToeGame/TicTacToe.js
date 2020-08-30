cc996a22384e955955cc7b925029f1d30e29f48a
gajanan15 <gajanandesai97@gmail.com>
2020-08-30 22:15:00 +0530

    [Gajanan]ADDED : Functionality For Resetting Game Board

--- TicTacToeGame/TicTacToe.js
+++ TicTacToeGame/TicTacToe.js
@@ -1,2 +1,22 @@
 console.log('Welcome To Tic Tac Toe Game');
+const ROWS = 3;
+const COLUMNS = 3;
+const PLAYER = 0;
+const TOTAL_MOVE = 9;
+let boardOfGame = [];
+let i;
+let j;
+class TicTacToeGame {
+    constructor() {
+        this.resettingBoard = () => {
+            for (i = 0; i < ROWS; i++) {
+                boardOfGame[i] = [];
+                for (j = 0; j < COLUMNS; j++) {
+                    boardOfGame[i][j] = "-";
+                }
+            }
+        };
+    }
+}
+new TicTacToeGame().resettingBoard();
 //# sourceMappingURL=TicTacToe.js.map
--- TicTacToeGame/TicTacToe.ts
+++ TicTacToeGame/TicTacToe.ts
@@ -1,1 +1,24 @@
+
 console.log('Welcome To Tic Tac Toe Game');
+
+const ROWS = 3;
+const COLUMNS = 3;
+const PLAYER = 0;
+const TOTAL_MOVE = 9;
+
+let boardOfGame: any[][] = [];
+let i;
+let j;
+
+class TicTacToeGame {
+    resettingBoard = () => {
+        for (i = 0; i < ROWS; i++) {
+            boardOfGame[i] = [];
+            for (j = 0; j < COLUMNS; j++) {
+                boardOfGame[i][j] = "-";
+            }
+        }
+    }
+}
+
+new TicTacToeGame().resettingBoard();
