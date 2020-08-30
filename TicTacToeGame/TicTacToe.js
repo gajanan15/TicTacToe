a6a924dad2e106bce6f858bceb4ed8c98be585a5
gajanan15 <gajanandesai97@gmail.com>
2020-08-30 22:40:51 +0530

    [Gajanan]ADDED : Checking Winning Condition for Player Move

--- TicTacToeGame/TicTacToe.js
+++ TicTacToeGame/TicTacToe.js
@@ -1,11 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const process_1 = require("process");
+var readlineSync = require('readline-sync');
 console.log('Welcome To Tic Tac Toe Game');
 const ROWS = 3;
 const COLUMNS = 3;
 const PLAYER = 0;
-const TOTAL_MOVE = 9;
+const TOTAL_MOVE = (ROWS * COLUMNS);
 let boardOfGame = [];
 let RandomValue = Math.round(Math.random()) % 2;
 var PLAYER_SYMBOL;
+let counter = 0;
 let i;
 let j;
 class TicTacToeGame {
@@ -45,10 +50,64 @@ class TicTacToeGame {
                 console.log("===============");
             }
         };
+        this.playGame = () => {
+            console.log("count : " + counter);
+            while (counter < TOTAL_MOVE) {
+                let row = readlineSync.question('Enter Player Row');
+                let col = readlineSync.question('Enter Player Col');
+                if (boardOfGame[row][col] != PLAYER_SYMBOL) {
+                    boardOfGame[row][col] = PLAYER_SYMBOL;
+                    this.displayGameBoard();
+                    this.winAtRowPosition(PLAYER_SYMBOL);
+                    this.winAtColPosition(PLAYER_SYMBOL);
+                    this.winAtDia(PLAYER_SYMBOL);
+                    counter++;
+                }
+                else {
+                    console.log("Invalid");
+                }
+            }
+        };
+        this.winAtRowPosition = (symbol) => {
+            console.log("Row: " + symbol);
+            for (let r = 0; r < ROWS; r++) {
+                for (let c = 0; c < COLUMNS; c++) {
+                    if (boardOfGame[r][c] == boardOfGame[r][c + 1] && boardOfGame[r][c + 1] == boardOfGame[r][c + 2] && boardOfGame[r][c] == symbol) {
+                        console.log(symbol + "Win");
+                        process_1.exit();
+                    }
+                }
+            }
+        };
+        this.winAtColPosition = (symbol) => {
+            if (boardOfGame[0][0] == boardOfGame[1][0] && boardOfGame[1][0] == boardOfGame[2][0] && boardOfGame[0][0] == symbol) {
+                console.log(symbol + "Win");
+                process_1.exit();
+            }
+            else if (boardOfGame[0][1] == boardOfGame[1][1] && boardOfGame[1][1] == boardOfGame[2][1] && boardOfGame[0][1] == symbol) {
+                console.log(symbol + "Win");
+                process_1.exit();
+            }
+            else if (boardOfGame[0][2] == boardOfGame[1][2] && boardOfGame[1][2] == boardOfGame[2][2] && boardOfGame[0][2] == symbol) {
+                console.log(symbol + "Win");
+                process_1.exit();
+            }
+        };
+        this.winAtDia = (symbol) => {
+            if ((boardOfGame[0][0] == boardOfGame[1][1]) && (boardOfGame[1][1] == boardOfGame[2][2]) && boardOfGame[0][0] == symbol) {
+                console.log(symbol + "Win");
+                process_1.exit();
+            }
+            else if ((boardOfGame[0][2] == boardOfGame[1][1]) && (boardOfGame[1][1] == boardOfGame[2][0]) && boardOfGame[0][2] == symbol) {
+                console.log(symbol + "Win");
+                process_1.exit();
+            }
+        };
     }
 }
 new TicTacToeGame().resettingBoard();
 new TicTacToeGame().assignedSymbol();
 new TicTacToeGame().whoWillPlayFirst();
 new TicTacToeGame().displayGameBoard();
+new TicTacToeGame().playGame();
 //# sourceMappingURL=TicTacToe.js.map
--- TicTacToeGame/TicTacToe.ts
+++ TicTacToeGame/TicTacToe.ts
@@ -1,14 +1,17 @@
+import { exit } from "process";
 
+var readlineSync = require('readline-sync');
 console.log('Welcome To Tic Tac Toe Game');
 
 const ROWS = 3;
 const COLUMNS = 3;
 const PLAYER = 0;
-const TOTAL_MOVE = 9;
+const TOTAL_MOVE = (ROWS * COLUMNS);
 
 let boardOfGame: any[][] = [];
 let RandomValue = Math.round(Math.random()) % 2;
 var PLAYER_SYMBOL;
+let counter = 0;
 let i;
 let j;
 
@@ -55,11 +58,69 @@ class TicTacToeGame {
         }
     }
 
+    playGame = () => {
+        console.log("count : " + counter)
+        while (counter < TOTAL_MOVE) {
+            let row = readlineSync.question('Enter Player Row');
+            let col = readlineSync.question('Enter Player Col');
+            if (boardOfGame[row][col] != PLAYER_SYMBOL) {
+                boardOfGame[row][col] = PLAYER_SYMBOL
+                this.displayGameBoard();
+                this.winAtRowPosition(PLAYER_SYMBOL);
+                this.winAtColPosition(PLAYER_SYMBOL);
+                this.winAtDia(PLAYER_SYMBOL);
+                counter++;
+            }
+            else {
+                console.log("Invalid");
+            }
+
+        }
+    }
+
+    winAtRowPosition = (symbol) => {
+        console.log("Row: " + symbol)
+        for (let r = 0; r < ROWS; r++) {
+            for (let c = 0; c < COLUMNS; c++) {
+                if (boardOfGame[r][c] == boardOfGame[r][c + 1] && boardOfGame[r][c + 1] == boardOfGame[r][c + 2] && boardOfGame[r][c] == symbol) {
+                    console.log(symbol + "Win");
+                    exit();
+                }
+            }
+        }
+    }
+
+    winAtColPosition = (symbol) => {
+        if (boardOfGame[0][0] == boardOfGame[1][0] && boardOfGame[1][0] == boardOfGame[2][0] && boardOfGame[0][0] == symbol) {
+            console.log(symbol + "Win");
+            exit();
+        } else if (boardOfGame[0][1] == boardOfGame[1][1] && boardOfGame[1][1] == boardOfGame[2][1] && boardOfGame[0][1] == symbol) {
+            console.log(symbol + "Win");
+            exit();
+        } else if (boardOfGame[0][2] == boardOfGame[1][2] && boardOfGame[1][2] == boardOfGame[2][2] && boardOfGame[0][2] == symbol) {
+            console.log(symbol + "Win");
+            exit();
+        }
+    }
+
+    winAtDia = (symbol) => {
+        if ((boardOfGame[0][0] == boardOfGame[1][1]) && (boardOfGame[1][1] == boardOfGame[2][2]) && boardOfGame[0][0] == symbol) {
+            console.log(symbol + "Win")
+            exit();
+        }
+        else if ((boardOfGame[0][2] == boardOfGame[1][1]) && (boardOfGame[1][1] == boardOfGame[2][0]) && boardOfGame[0][2] == symbol) {
+            console.log(symbol + "Win")
+            exit();
+        }
+    }
+
 }
 
 new TicTacToeGame().resettingBoard();
 new TicTacToeGame().assignedSymbol();
 new TicTacToeGame().whoWillPlayFirst();
 new TicTacToeGame().displayGameBoard();
+new TicTacToeGame().playGame();
+
 
 
